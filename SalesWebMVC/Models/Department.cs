e4b9d338f84d2d1378a4c3487a16f13aa4d0ac2c
Antony de Lara <antonydelara.lima09@gmail.com>
2023-08-24 14:57:51 -0400

    First Model-Controller-View from Department

---
+++ SalesWebMVC/Models/Department.cs
@@ -0,0 +1,25 @@
+﻿namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        /*
+        public void addSeller(Seller seler)
+        {
+
+        }
+        */
+        public double totalSales(DateTime initial, DateTime final)
+        {
+            return 0;
+        }
+    }
+}
